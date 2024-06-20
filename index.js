fc21069313b2397fa8056ff32290c2fc659e6159
Synetic1 <syneticslz@gmail.com>
2024-06-20 16:16:48 +0100

    new pwrd

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.post('/send-email-smtp', async (req, res) => {
         service: 'gmail',
         auth: {
             user: 'voltmailerhelp@gmail.com',
-            pass: 'Plutoandmars'
+            pass: 'PlutoandMars1!'
         }
     });
 
