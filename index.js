3e565ef5e044a3dd8046715e5f66b46b777aeef4
Synetic1 <syneticslz@gmail.com>
2024-06-22 17:26:43 +0100

    update mongo call

--- index.js
+++ index.js
@@ -43,9 +43,10 @@ app.use(session({
 }));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err));
+
 
 // Helper functions
 async function findCustomer(email) {
