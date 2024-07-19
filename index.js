c9dd2cb2de42248cc96cfd61d0517d61a60b198a
SyneticSLZ <116673411+SyneticSLZ@users.noreply.github.com>
2024-07-19 11:26:19 +0100

    Update index.js
    updated input

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ async function AddMessageToThread(ThreadID, website_content, user_pitch, To, Me)
             ThreadID,
             {
                 role: "user",
-                content: `I'm selling ${user_pitch}, This is the data I have on the company and what they do from their website ${website_content}. And this is the user's pitch: ${user_pitch}. This is the name you should use to address them in the email ${To} from my name, ${Me}. I want you to create the email where the first line is the subject line and then the greeting and content follows. I wnat thie output to be professional, so no blank fieldse. [company name ] ect.`
+                content: `This is the data I have on the company I'm sending this email to ${website_content}. This is the pitch I am going to use: ${user_pitch}. You should address the reciever of this email with the name ${To}. You should also state that it was sent by me using my name: ${Me}.`
             }
         );
         console.log("Message added");
