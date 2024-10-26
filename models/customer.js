8b82c5b3efb864f8a4f86816e9b1a041a68ea4d8
Synetic1 <syneticslz@gmail.com>
2024-10-26 19:51:20 +0100

    email

--- models/customer.js
+++ models/customer.js
@@ -18,6 +18,17 @@
 
 const mongoose = require('mongoose');
 
+const emailSchema = new mongoose.Schema({
+    recipientEmail: String,
+    subject: String,
+    messageId: String,
+    threadId: String,
+    sentTime: Date,
+    status: { type: String, enum: ['sent', 'bounced', 'delivered'], default: 'sent' },
+    bounces: { type: Boolean, default: false },
+    responseCount: { type: Number, default: 0 }
+});
+
 const campaignSchema = new mongoose.Schema({
     campaignName: String,
     template: String,
