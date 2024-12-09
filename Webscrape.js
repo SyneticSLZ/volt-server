1e7722a5f84e768a99799bd6a75e7565e9726d8c
Synetic1 <syneticslz@gmail.com>
2024-12-09 21:52:27 +0000

    path

--- Webscrape.js
+++ Webscrape.js
@@ -33,7 +33,7 @@ class UltimateCompanyIntelligenceScraper {
     async init() {
         this.browser = await puppeteer.launch({
             headless: true, 
-            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+            executablePath: '/opt/render/.cache/puppeteer/chrome/linux-131.0.6778.87/chrome',
             args: [
                 '--no-sandbox', 
                 '--disable-setuid-sandbox', 
